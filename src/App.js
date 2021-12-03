55440c72f01fffaa09c3f660449e7d57885d2952
JacksonJ97 <jacksonjude97@gmail.com>
2021-12-02 22:14:14 -0500

    created base folder and file structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+// Components
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
+// Styles
+import GlobalStyles from "./theme/GlobalStyles";
+
 const App = () => {
-  return <div>Hello World!</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <Header />
+      <Main />
+      <Footer />
+      <div>Hello World!</div>
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+// Components
+import Nav from "./Nav/Nav";
+
+const Header = () => {
+  return (
+    <header>
+      <Nav />
+    </header>
+  );
+};
+
+export default Header;
