7c3c057e808172a9fdc9506282ef4ef5f5118a59
JacksonJ97 <jacksonjude97@gmail.com>
2021-12-05 19:49:26 -0500

    changed section header styles

--- src/components/Utilities/SectionHeader/index.js
+++ src/components/Utilities/SectionHeader/index.js
@@ -3,7 +3,9 @@ import styled from "styled-components";
 // Styles
 const Header = styled.h2`
   font-size: 2rem;
-  margin: 3em 0 2em 0;
+  margin: calc(55px + 1.5em) 0 1.5em 0;
+  border-bottom: 1px solid #ffd369;
+  padding-bottom: 0.15em;
 `;
 
 const SectionHeader = ({ text }) => {
