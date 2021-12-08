e46d6a4b60a69470be8e7b1fd68be2c24fff65ee
JacksonJ97 <jacksonjude97@gmail.com>
2021-12-08 05:10:10 -0500

    changed the tech box component's style

--- src/components/Utilities/Card/TechBox/index.js
+++ src/components/Utilities/Card/TechBox/index.js
@@ -4,13 +4,11 @@ import styled from "styled-components";
 const Wrapper = styled.li`
   display: inline-block;
   border: 1px solid #222831;
-  background-color: #eeeeee;
-  color: #222831;
+  color: #eeeeee;
   font-size: 0.875rem;
   padding: 0.5em;
   margin: 0.25em 0.5em 0.25em 0;
   border-radius: 6px;
-  box-shadow: 0 0 12px 0 rgba(0, 0, 0, 0.2);
 `;
 
 const TechBox = ({ text }) => {
