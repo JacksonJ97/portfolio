7c757a5fe9d8d01b8bd850b0772f9c057d2af1f1
JacksonJ97 <jacksonjude97@gmail.com>
2021-12-14 19:40:26 -0500

    edited the content

--- src/data.js
+++ src/data.js
@@ -9,7 +9,7 @@ const projectData = [
   {
     title: "CV Application",
     description:
-      "A resume builder application that takes the user's inputs and displays a formatted resume. Users can add or delete any number of experiences and education. Users can also load an example resume.",
+      "A resume builder that takes the user's inputs and displays a formatted resume. Users can add/delete any number of experience and education fields. Users can also load an example resume.",
     technologies: ["HTML", "CSS", "JavaScript", "React"],
     liveLink: "https://jacksonj97.github.io/cv-application/",
     repoLink: "https://github.com/JacksonJ97/cv-application",
@@ -18,7 +18,7 @@ const projectData = [
   {
     title: "Portfolio",
     description:
-      "A portfolio website to share some details about myself, showcase my projects, and to give a resource for people to connect with me!",
+      "A portfolio website where I can showcase my projects and where people can learn more about me. This also serves as a resource for people to connect with me!",
     technologies: ["HTML", "CSS", "JavaScript", "React"],
     liveLink: "https://jacksonj97.github.io/portfolio/",
     repoLink: "https://github.com/JacksonJ97/portfolio",
@@ -27,7 +27,7 @@ const projectData = [
   {
     title: "Pokemon Memory Game",
     description:
-      "A memory game where a user's score is increased depending on the number of unique pokemons clicked. If users click on the same pokemon their score is resetted.",
+      "A memory game where a user's score is increased depending on the number of unique pokemons clicked. If users click on the same pokemon their score is reset.",
     technologies: ["HTML", "CSS", "JavaScript", "React"],
     liveLink: "https://jacksonj97.github.io/memory-card/",
     repoLink: "https://github.com/JacksonJ97/memory-card",
@@ -36,7 +36,7 @@ const projectData = [
   {
     title: "Weather Application",
     description:
-      "A weather application that takes a user's input of a city and displays the corresponding weather information. Users can also switch the units to celsius or fahrenheit.",
+      "A weather application that takes a user's input of a city and displays the corresponding weather information. Users can also switch the units to either imperial or metric.",
     technologies: ["HTML", "CSS", "JavaScript"],
     liveLink: "https://jacksonj97.github.io/weather-app/",
     repoLink: "https://github.com/JacksonJ97/weather-app",
@@ -45,7 +45,7 @@ const projectData = [
   {
     title: "Jack's Cafe",
     description:
-      "A fake restaurant website that contains a tabbing system to switch between the contents of the home, menu, and contact sections.",
+      "A restaurant website suitable for a small business that contains a tabbing system to switch between the contents of the home, menu, and contact sections.",
     technologies: ["HTML", "CSS", "JavaScript"],
     liveLink: "https://jacksonj97.github.io/restaurant-page/",
     repoLink: "https://github.com/JacksonJ97/restaurant-page",
