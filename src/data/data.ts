d947bc01129bf961b034da79112d31a3b690bdd0
Jackson <jacksonjude97@gmail.com>
2025-03-30 18:17:06 -0400

    remove old projects

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,9 +1,6 @@
 import spaceImg from "../assets/images/space-icon.svg";
 import clothingImg from "../assets/images/clothing-icon.svg";
 import cvImg from "../assets/images/cv-app-icon.svg";
-import pokemonImg from "../assets/images/pokemon-icon.svg";
-import weatherImg from "../assets/images/weather-icon.svg";
-import portfolioImg from "../assets/images/portfolio-icon.svg";
 
 export const data = [
   {
@@ -35,35 +32,5 @@ export const data = [
     liveLink: "https://jacksonj97.github.io/cv-application/",
     repoLink: "https://github.com/JacksonJ97/cv-application",
     img: cvImg,
-  },
-  {
-    id: 4,
-    title: "Pokemon Memory Game",
-    description:
-      "A memory game where a user's score is increased depending on the number of unique pokemons clicked. If users click on the same pokemon their score is reset.",
-    technologies: ["HTML", "CSS", "JavaScript", "React"],
-    liveLink: "https://jacksonj97.github.io/memory-card/",
-    repoLink: "https://github.com/JacksonJ97/memory-card",
-    img: pokemonImg,
-  },
-  {
-    id: 5,
-    title: "Weather Application",
-    description:
-      "A weather application that takes a user's input of a city and displays the corresponding weather information. Users can also switch the units to either imperial or metric.",
-    technologies: ["HTML", "CSS", "JavaScript"],
-    liveLink: "https://jacksonj97.github.io/weather-app/",
-    repoLink: "https://github.com/JacksonJ97/weather-app",
-    img: weatherImg,
-  },
-  {
-    id: 6,
-    title: "Portfolio",
-    description:
-      "A portfolio website where I can showcase my projects and where people can learn more about me. This also serves as a resource for people to connect with me!",
-    technologies: ["HTML", "CSS", "JavaScript", "React"],
-    liveLink: "https://jacksonj97.github.io",
-    repoLink: "https://github.com/JacksonJ97/jacksonj97.github.io",
-    img: portfolioImg,
   },
 ];
