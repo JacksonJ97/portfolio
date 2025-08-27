a2572e635dab90a4fdd50977adec1a527d617163
Jackson <jacksonjude97@gmail.com>
2025-08-26 20:07:55 -0400

    header tweaks

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ function Link({ href, text }: { href: string; text: string }) {
   return (
     <a
       href={href}
-      className="font-fira-code text-(--text-color) uppercase transition-colors hover:text-(--primary-color)"
+      className="font-fira-code text-base text-(--text-color) uppercase transition-colors hover:text-(--primary-color)"
     >
       {text}
     </a>
