672d78d1684eaf07fd3cd0e8f93c0a0f156d3329
Jackson <jacksonjude97@gmail.com>
2025-08-26 20:06:28 -0400

    work section tweaks

--- src/content/Work/index.tsx
+++ src/content/Work/index.tsx
@@ -1,12 +1,12 @@
 export default function Work() {
   return (
     <section id="work" className="scroll-mt-24">
-      <h2 className="font-fira-code mb-4 text-2xl font-medium uppercase">
+      <h2 className="font-fira-code mb-4 text-2xl font-medium text-(--text-color) uppercase">
         Work
       </h2>
 
       <div className="min-xs:flex-row min-xs:items-center mb-1 flex flex-col justify-between gap-1">
-        <h3 className="font-medium text-(--text-color)">PheedLoop</h3>
+        <h3 className="text-base font-medium text-(--text-color)">PheedLoop</h3>
         <p className="text-sm text-(--text-muted-color)">Feb 2022 - Mar 2025</p>
       </div>
 
